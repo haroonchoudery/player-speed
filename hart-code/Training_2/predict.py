e97c430a5229c62cf8a220acc1c9af5626cdeaa5
Haroon Choudery <haroon.choudery@gmail.com>
2017-11-15 22:20:33 -0500

    changed prediction image

--- hart-code/Training_2/predict.py
+++ hart-code/Training_2/predict.py
@@ -6,11 +6,11 @@ import os
 import tensorflow as tf
 
 
-image_path = '/Users/haroonchoudery/Desktop/frame_000020.jpg'
+image_path = '/Users/haroonchoudery/Desktop/frame_000228.jpg'
 weights_path = os.path.join('checkpoint', 'mobilenet_custom_marker_3_1024.h5py')
 do_load_model = False
 
-labels = [0.5185546875,0.546875,0.431640625,0.6927083333333334,0.0244140625,0.6440972222222222,0.1787109375,0.5104166666666666]
+labels = [0.3935546875,0.8524305555555556,0.24609375,0.6944444444444444,0.642578125,0.6197916666666666,0.83984375,0.7534722222222222]
 
 
 image = cv2.imread(image_path, 1)
