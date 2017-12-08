89853e6e718e8446b682ac1f2a57e76df40b226f
Haroon Choudery <haroon.choudery@gmail.com>
2017-12-08 13:21:31 -0500

    small change

--- hart-code/Training_2/mobilenet_custom.py
+++ hart-code/Training_2/mobilenet_custom.py
@@ -23,7 +23,7 @@ class MobileNetCustom():
 		model.add(Conv2D(filter_2 * 2,kernel_size=(1,1),strides=(1,1), padding='same'))
 		model.add(BatchNormalization())
 		model.add(Activation('relu'))
-		
+
 	def final_conv_block(self):
 		model = self.model
 		model.add(SeparableConv2D(512,kernel_size=(3,3), strides=(2,2),padding='same'))#
@@ -38,7 +38,7 @@ class MobileNetCustom():
 		model.add(Conv2D(1024,kernel_size=(1,1),strides=(1,1), padding='same'))
 		model.add(BatchNormalization())
 		model.add(Activation('relu'))
-		
+
 	def separable_filters(self):
 		model = self.model
 		for i in range(5):
@@ -47,23 +47,23 @@ class MobileNetCustom():
 			model.add(Activation('relu'))
 			model.add(Conv2D(512,kernel_size=(1,1),strides=(1,1), padding='same'))
 			model.add(BatchNormalization())
-			model.add(Activation('relu'))    
-	
+			model.add(Activation('relu'))
+
 	def pool_and_classify(self):
 		model = self.model
 		model.add(AveragePooling2D(pool_size=(7,7),strides=(1,1)))
 		model.add(Flatten())
 		model.add(Dense(self.classes))
 		#model.add(Activation('relu')) #was 'softmax'
-		
+
 	def __init__(self, size=(224,224,3), classes=(1000)):
 		self.classes = classes
 		self.create(size)
-	
+
 	def create(self, size):
 		self.model = Sequential()
 		self.model.add(Conv2D(32,kernel_size=(3,3),strides=(2,2), padding='same', input_shape=size))
-		self.mobile_block(32,64)          
+		self.mobile_block(32,64)
 		self.mobile_block(128,128)
 		self.mobile_block(256,256)
 		self.separable_filters()
