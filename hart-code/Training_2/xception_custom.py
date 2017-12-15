3401a2d85f46106cd642d33bb45929f53f6c9cee
sumopal <sp4521@nyu.edu>
2017-12-15 11:04:49 -0500

    added new xception model

---
+++ hart-code/Training_2/xception_custom.py
@@ -0,0 +1,21 @@
+from keras.layers import Input, Reshape, Conv2D, SeparableConv2D, BatchNormalization, GlobalAveragePooling2D, GlobalMaxPooling2D, AveragePooling2D
+from keras.models import Model, Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras import backend as K
+
+
+import keras.applications.mobilenet as mobilenet
+from keras.applications.mobilenet import DepthwiseConv2D#, _depthwise_conv_block, _conv_block
+class InceptionResNetV2_custom():
+    def __init__(self,size=(256,144,3),classes=(1000)):
+        self.classes = classes
+        self.new_model(size)
+
+    def new_model(self,size):
+        self.prev_model = keras.applications.xception.Xception(include_top=False, weights=None, input_tensor=None, input_shape=size, pooling=None, classes=1000)
+        self.x = self.prev_model.output
+        self.x=AveragePooling2D(pool_size=(2,2), strides=(1, 1))(self.x)
+        self.x = Flatten()(self.x)
+        self.x = Dense(self.classes)(self.x)
+        self.model = Model(inputs=self.prev_model.input,outputs=self.x)
