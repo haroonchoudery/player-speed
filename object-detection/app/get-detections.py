864274bdc5adc660ee7c5ae7965aa0316553f29e
Ubuntu <ubuntu@ip-172-31-14-225.ec2.internal>
2018-03-13 23:20:43 +0000

    changed get-detections

--- object-detection/app/get-detections.py
+++ object-detection/app/get-detections.py
@@ -18,27 +18,27 @@ video = os.path.join(VIDEO_DIR, VIDEO_FILE)
 
 def to_mot_format(frame_idx, coord):
     """
-    Input coordinates: 
+    Input coordinates:
     (y1, x1, y2, x2)
-    
-    Output coordinates: 
+
+    Output coordinates:
     (frame, id, bb_left, bb_top, bb_width, bb_height, -1, -1, -1, -1)
     """
     padding = np.array([-1, -1, -1, -1])
-    
+
     coord = np.insert(coord, 0, frame_idx)
     coord = np.insert(coord, 1, -1)
     coord = np.append(coord, padding)
-    
+
     width = coord[5] - coord[3]
     height = coord[4] - coord[2]
-    
+
     coord[4] = width
     coord[5] = height
 
     # Rearrange coordinates
     coord = coord[[0, 1, 3, 2, 4, 5, 6, 7, 8, 9]]
-    
+
     return coord
 
 def get_detections_frame(image, frame_idx):
@@ -47,56 +47,55 @@ def get_detections_frame(image, frame_idx):
         # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
         GPU_COUNT = 1
         IMAGES_PER_GPU = 1
-        
+
     config = InferenceConfig()
-    
+
     # Create model object in inference mode.
     model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
 
     # Load weights trained on MS-COCO
     model.load_weights(COCO_MODEL_PATH, by_name=True)
-    
+
     results = model.detect([image], verbose=0)
     rois = results[0]['rois']
-    
+
     detections = np.zeros([len(rois), 10])
-    
+
     for idx, coord in enumerate(rois):
         detections[idx] = to_mot_format(frame_idx, coord)
-    
+
     return detections
 
 def get_detections_video(video):
     """
-    Get ROI detections from video using Mask-R-CNN and save in 
+    Get ROI detections from video using Mask-R-CNN and save in
     MOTChallenge format
-    """    
+    """
     camera = cv2.VideoCapture(video)
     num_frames = int(camera.get(cv2.CAP_PROP_FRAME_COUNT))
     success,image = camera.read()
     count = 0
     success = True
 
-    
+
     while success:
         success,image = camera.read()
         print("Processing image {}".format(count))
         detection = get_detections_frame(image, count)
-        print(detection)
+
         if count == 0:
             detections = detection
         else:
             detections = np.concatenate((detections, detection))
-        
-        if count == 3:
-            break
-            
+
+	print("Detections extracted from image {}".format(count))
+
         count += 1
-        
+
     return detections
-    
-    
-if __name__ == '__main__':    
+
+
+if __name__ == '__main__':
     detections = get_detections_video(video)
 
     with open('detections.pickle', 'wb') as handle:
